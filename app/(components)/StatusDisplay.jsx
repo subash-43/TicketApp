c66190fbd565b90d65ebd29324ed0a0fc9d780d8
subash <subash43it@proton.me>
2024-07-21 15:10:27 +0000

    time update && color update && text-update

--- app/(components)/StatusDisplay.jsx
+++ app/(components)/StatusDisplay.jsx
@@ -1,9 +1,26 @@
-const StatusDisplay = () => {
-  return (
-    <span className='inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200'>
-done
-    </span>
-  )
-}
+const StatusDisplay = ({ status }) => {
 
-export default StatusDisplay
+  
+  const getColor = (status) => {
+    let color = "bg-slate-200";
+    switch (status) {
+      case "done":
+        color = "bg-green-200";
+        return color;
+      case "started":
+        color = "bg-yellow-200";
+        return color;
+      case "not started":
+        color = "bg-red-200";
+        return color;
+    }
+    return color;
+  };
+  return (
+    <span className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(status)}`}>
+      {status}
+    </span>
+  );
+};
+
+export default StatusDisplay;
