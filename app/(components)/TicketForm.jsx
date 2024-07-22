9a443f70c12079176db4b630b5da29ba88638616
subash-43 <subash43it@proton.me>
2024-07-22 12:07:50 +0530

    Tickets ready

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -1,16 +1,33 @@
 "use client";
-
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import React, { useState } from "react";
 
-const TicketForm = () => {
+const TicketForm = ({ ticket }) => {
+  const EDITMODE = ticket._id === "new" ? false : true;
+  const router = useRouter();
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "not started",
+    category: "Hardware Problem",
+  };
 
-  const router = useRouter()
+  if (EDITMODE) {
+    startingTicketData["title"] = ticket.title;
+    startingTicketData["description"] = ticket.description;
+    startingTicketData["priority"] = ticket.priority;
+    startingTicketData["progress"] = ticket.progress;
+    startingTicketData["status"] = ticket.status;
+    startingTicketData["category"] = ticket.category;
+  }
+
+  const [formData, setFormData] = useState(startingTicketData);
 
   const handleChange = (e) => {
     const value = e.target.value;
     const name = e.target.name;
-    
 
     setFormData((preState) => ({
       ...preState,
@@ -20,42 +37,52 @@ const TicketForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch('/api/Tickets',{
-      method: 'POST',
-      body: JSON.stringify({formData}),
-      'content-type': "application/json"
-    })
-    if(!res.ok){
-      throw new Error(
-        "Failed to create Ticket."
-      )}
-      router.refresh()
-      router.push('/')
 
+    if (EDITMODE) {
+      const res = await fetch(`/api/Tickets/${ticket._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ formData }),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to update ticket");
+      }
+    } else {
+      const res = await fetch("/api/Tickets", {
+        method: "POST",
+        body: JSON.stringify({ formData }),
+        //@ts-ignore
+        "Content-Type": "application/json",
+      });
+      if (!res.ok) {
+        throw new Error("Failed to create ticket");
+      }
+    }
+
+    router.refresh();
+    router.push("/");
   };
 
-  const startingTicketData = {
-    title: "",
-    description: "",
-    priority: 1,
-    progress: 0,
-    status: "not started",
-    category: "Hardware proplem",
-  };
-
-  const [formData, setFormData] = useState(startingTicketData);
+  const categories = [
+    "Hardware Problem",
+    "Software Problem",
+    "Application Deveopment",
+    "Project",
+  ];
 
   return (
-    <div className="flex justify-center">
+    <div className=" flex justify-center">
       <form
-        className="flex flex-col gap-3 w-1/2"
-        method="post"
         onSubmit={handleSubmit}
+        method="post"
+        className="flex flex-col gap-3 w-1/2"
       >
-        <h3>Create your Ticket</h3>
+        <h3>{EDITMODE ? "Update Your Ticket" : "Create New Ticket"}</h3>
         <label>Title</label>
         <input
-          id="tittle"
+          id="title"
           name="title"
           type="text"
           onChange={handleChange}
@@ -71,16 +98,17 @@ const TicketForm = () => {
           value={formData.description}
           rows="5"
         />
-
-        <label>Categories</label>
+        <label>Category</label>
         <select
           name="category"
           value={formData.category}
           onChange={handleChange}
         >
-          <option value="Harward Proplem">Hardware Proplem</option>
-          <option value="Software Proplem">Software Proplem</option>
-          <option value="Project">Project</option>
+          {categories?.map((category, _index) => (
+            <option key={_index} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
 
         <label>Priority</label>
@@ -137,18 +165,21 @@ const TicketForm = () => {
           id="progress"
           name="progress"
           value={formData.progress}
-          min={"0"}
-          max={"100"}
+          min="0"
+          max="100"
           onChange={handleChange}
         />
         <label>Status</label>
         <select name="status" value={formData.status} onChange={handleChange}>
-          <option value="not Started">Not Started</option>
+          <option value="not started">Not Started</option>
           <option value="started">Started</option>
           <option value="done">Done</option>
         </select>
-
-        <input type="submit" className="btn" value={"Create Ticket"} />
+        <input
+          type="submit"
+          className="btn max-w-xs"
+          value={EDITMODE ? "Update Ticket" : "Create Ticket"}
+        />
       </form>
     </div>
   );
--- app/api/Tickets/[id]/route.js
+++ app/api/Tickets/[id]/route.js
@@ -1,14 +1,48 @@
 import Ticket from "@/app/(models)/Tickets";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req, { params }) {
-    try {
-      const { id } = params;
-  
-      await Ticket.findByIdAndDelete(id);
-      return NextResponse.json({ message: "Ticket Deleted" }, { status: 200 });
-    } catch (error) {
-      console.log(error);
-      return NextResponse.json({ message: "Error", error }, { status: 500 });
-    }
+export async function GET(req, { params }) {
+  try {
+    const { id } = params
+    const foundTicket = await Ticket.findOne({ _id: id });
+    return NextResponse.json({ foundTicket }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
+
+}
+
+export async function DELETE(req, { params }) {
+  try {
+    const { id } = params;
+
+    await Ticket.findByIdAndDelete(id);
+    return NextResponse.json({ message: "Ticket Deleted" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
+
+export async function PUT(req, { params }) {
+
+  
+
+  try {
+    const { id } = params;
+
+    console.log(id)
+
+    const body = await req.json();
+    const ticketData = body.formData;
+
+    const updateTicketData = await Ticket.findByIdAndUpdate(id, {
+      ...ticketData
+    })
+
+    return NextResponse.json({ message: "Ticket Updated" }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+}
